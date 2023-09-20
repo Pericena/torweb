dc4e0355a467680374d68dcedcda58735a49130e
Pericena <pericena12@gmail.com>
2023-09-20 05:51:02 -0400

    webtorv1

---
+++ asset/js/main.js
@@ -0,0 +1,47 @@
+var intervalID = window.setInterval(updateScreen, 200);
+var c = document.getElementById("console");
+
+var txt = [
+    "FORCE: XX0022. ENCYPT://000.222.2345",
+    "TRYPASS: ********* AUTH CODE: ALPHA GAMMA: 1___ PRIORITY 1",
+    "RETRY: REINDEER FLOTILLA",
+    "Z:> /FALKEN/GAMES/TICTACTOE/ EXECUTE -PLAYERS 0",
+    "=====================================",
+    "Priority 1 // local / scanning...",
+    "scanning ports...",
+    "BACKDOOR FOUND (23.45.23.12.00000000)",
+    "BACKDOOR FOUND (13.66.23.12.00110000)",
+    "BACKDOOR FOUND (13.66.23.12.00110044)",
+    "...",
+    "...",
+    "...",
+    "BRUTE.EXE -r -z",
+    "...locating vulnerabilities...",
+    "...vulnerabilities found...",
+    "MCP/> INGRESANDO A LA COMUNIDAD ",
+    "Grupo: https://www.facebook.com/groups/nucleolinux.uagrm",
+    "Facebook: https://www.facebook.com/nucleolinuxuagrm/",
+    "by: __ Luishiño",
+    "...",
+    "...",
+    "...",
+    "...",
+];
+
+var docfrag = document.createDocumentFragment();
+
+function updateScreen() {
+  //Shuffle the "txt" array
+  txt.push(txt.shift());
+  //Rebuild document fragment
+  txt.forEach(function(e) {
+    var p = document.createElement("p");
+    p.textContent = e;
+    docfrag.appendChild(p);
+  });
+  //Clear DOM body
+  while (c.firstChild) {
+    c.removeChild(c.firstChild);
+  }
+  c.appendChild(docfrag);
+}
